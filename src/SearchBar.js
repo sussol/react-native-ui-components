0b4a44bcfea6d2a891f8b0ecff9273ed915e64d5
Edwin <edwin@sussol.net>
2016-10-19 12:43:50 +1300

    Initial commit

---
+++ src/SearchBar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/EvilIcons';
+
+const defaultStyles = StyleSheet.create({
+  container: {
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 5,
+  },
+  textInput: {
+    height: 40,
+    fontSize: 16,
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+  },
+});
+
+export class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputReference = null;
+  }
+
+  render() {
+    const { color, ...textInputProps } = this.props;
+    return (
+      <TouchableOpacity
+        style={[defaultStyles.container, { borderColor: color }]}
+        onPress={() => this.inputReference.focus()}
+      >
+        <Icon name="search" size={40} color={color} />
+        <TextInput
+          {...textInputProps}
+          ref={(reference) => (this.inputReference = reference)}
+          underlineColorAndroid="transparent"
+        />
+      </TouchableOpacity>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  color: React.PropTypes.string,
+  onChange: React.PropTypes.func,
+  style: TextInput.propTypes.style,
+};
+
+SearchBar.defaultProps = {
+  style: defaultStyles.textInput,
+};
